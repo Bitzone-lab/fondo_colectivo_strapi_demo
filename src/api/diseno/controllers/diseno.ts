555ecb51fb7e4f2029a598b8ae7a62b9234bfb0e
ertrii <ertriilive@gmail.com>
2025-05-22 14:52:24 -0500

    strapi started

---
+++ src/api/diseno/controllers/diseno.ts
@@ -0,0 +1,7 @@
+/**
+ * diseno controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::diseno.diseno');
---
+++ src/api/diseno/services/diseno.ts
@@ -0,0 +1,7 @@
+/**
+ * diseno service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::diseno.diseno');
